5fe42f8b6299234b1b59b4033e762d20327e7039
ajaotemitope5@gmail.com <122884770+Temitope3003@users.noreply.github.com>
2023-05-08 11:49:28 +0100

    web home page

---
+++ home.py
@@ -0,0 +1,13 @@
+home.py
+2*2
+print('hello, world')
+a= float(input('input the value of a'))
+print('5*3')
+addition = 10 + 20 + \
+
+           30 + 40 + \
+           50 + 60 + 70
+print('addition')
+
+
+2+2
